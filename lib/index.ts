ecadfd112836a0ef7143d61a6a5db486c4da9e98
Douglas Naphas <douglasnaphas@gmail.com>
2021-11-04 14:19:09 -0400

    Correct the name of the SGGetAtt output

--- lib/index.ts
+++ lib/index.ts
@@ -24,9 +24,9 @@ export class Stack extends cdk.Stack {
       value: bucket.bucketName,
     });
     const cfnSg = sg.node.defaultChild as ec2.CfnSecurityGroup;
-    new cdk.CfnOutput(this, "SGRef", {
+    new cdk.CfnOutput(this, "SGGetAtt", {
       value: cfnSg.getAtt("GroupId").toString(),
     });
-    
+
   }
 }
